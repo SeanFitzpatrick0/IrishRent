2aec7015b22993433c77c164cc8b779d50f57184
SeanFitz1997 <seanlkfitzpatrick@gmail.com>
2023-09-10 13:04:03 +0100

    Update google analytics version 4

--- IrishRent/lib/Analytics.ts
+++ IrishRent/lib/Analytics.ts
@@ -1,4 +1,4 @@
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export function initGA() {
 	if (process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID)
@@ -8,13 +8,8 @@ export function initGA() {
 
 export function logPageView() {
 	ReactGA.set({ page: window.location.pathname });
-	ReactGA.pageview(window.location.pathname);
 }
 
 export function logEvent(category = "", action = "") {
 	if (category && action) ReactGA.event({ category, action });
-}
-
-export function logException(description = "", fatal = false) {
-	if (description) ReactGA.exception({ description, fatal });
 }
